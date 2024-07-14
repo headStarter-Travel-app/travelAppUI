ad49d4b13c50e1ab7902799596ef2c55d48b58c9
namanNagelia <nagelia.naman@gmail.com>
2024-07-14 03:13:33 -0400

    table users

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,4 +1,4 @@
-import { Account, Client, ID } from "react-native-appwrite";
+import { Account, Client, Databases, ID } from "react-native-appwrite";
 
 export const appwriteConfig = {
   endpoint: "https://cloud.appwrite.io/v1",
@@ -17,15 +17,34 @@ client
   .setPlatform(appwriteConfig.platform); // Your application ID or bundle ID.
 
 const account = new Account(client);
+const databases = new Databases(client);
 
 // Register User
-export const CreateUser = (email: string, password: string) => {
-  account.create(ID.unique(), email, password).then(
+export const CreateUser = (
+  email: string,
+  password: string,
+  firstName: string,
+  lastName: string
+) => {
+  const userID = ID.unique();
+  const name = firstName + " " + lastName;
+  account.create(userID, email, password, name).then(
     function (response) {
       console.log(response);
     },
     function (error) {
       console.log(error);
+    }
+  );
+  const promise = databases.createDocument(
+    appwriteConfig.databaseId,
+    appwriteConfig.userCollectionId,
+    ID.unique(),
+    {
+      $id: userID,
+      email: email,
+      firstName: firstName,
+      lastName: lastName,
     }
   );
 };
