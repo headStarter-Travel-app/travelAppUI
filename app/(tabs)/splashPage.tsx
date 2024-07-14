46472df8cfb073724245f8dfd1462353a2a72585
ahmadbasyouni10 <ahmadnayef2004@gmail.com>
2024-07-13 22:40:00 -0400

    Created Register Page and handled backend

--- app/(tabs)/splashPage.tsx
+++ app/(tabs)/splashPage.tsx
@@ -15,7 +15,7 @@ const SplashPage = () => {
       <AppButton
         title="Sign Up With Email"
         onPress={() => {
-          CreateUser();
+          // Navigate to the sign up page
         }}
       />
     </View>
