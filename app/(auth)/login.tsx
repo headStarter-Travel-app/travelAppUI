49a1ed9d25c159496551b77f116c4d780c4f58cb
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-07-14 14:42:40 -0400

    Designed Login Page and handled logging in

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput, Alert } from "react-native";
+import AppButton from "@/components/usableOnes/button";
+import { LoginUser } from "@/lib/appwrite"; // Assuming you have a login function in your appwrite library
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
+import { Link, useRouter } from "expo-router";
+
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    try {
+      const session = await LoginUser(email, password);
+      if (session && session.$id) {
+        await AsyncStorage.setItem('userToken', session.$id);
+        Alert.alert("Login successful");
+        router.push('/(tabs)'); // Replace 'your-tabs-url' with the actual tabs URL
+      } else {
+        throw new Error("Invalid session object");
+      }
+    } catch (error: any) {
+      Alert.alert("Login failed", error.message);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={setPassword}
+      />
+
+      <AppButton title="Login" onPress={handleLogin} />
+      <Text style={styles.registerText}>
+        Don't have an account?{" "}
+        <Link href="/register" asChild>
+          <Text style={styles.registerTextBold}>Register</Text>
+        </Link>
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+  input: {
+    width: "50%",
+    borderWidth: 2,
+    borderColor: "gray",
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 5,
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  registerText: {
+    marginTop: 20,
+  },
+  registerTextBold: {
+    fontWeight: "bold",
+  },
+});
+
+export default LoginPage;
