7e50e52e36c5aec025d1b20f4d8ab5ecab68a78f
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-07-15 01:41:58 -0400

    dependencies

--- app/index.tsx
+++ app/index.tsx
@@ -6,7 +6,7 @@ import Spinner from 'react-native-loading-spinner-overlay';
 export default function Index() {
   const [isLogged, setIsLogged] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
-
+ 
   useEffect(() => {
     const checkLoginStatus = async () => {
       try {
@@ -24,7 +24,7 @@ export default function Index() {
   }, []);
 
   if (isLoading) {
-    return <Spinner visible={true} textContent={'Loading...'} textStyle={{color: "FFF"}} />;
+    return <Spinner visible={true} textContent={'Loading...'} textStyle={{color: "#FFF"}} />;
   }
 
   if (isLogged) {
