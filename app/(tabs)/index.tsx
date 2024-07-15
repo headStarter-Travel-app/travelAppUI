ce5de7d2a6490353ee5dc2a24221226f91ec4cf2
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-07-15 02:26:14 -0400

    Added Logout functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,28 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, Alert, View, Button } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { LogoutUser } from '@/lib/appwrite';
+import { Link, useRouter } from "expo-router";
+
 
 export default function HomeScreen() {
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    try {
+      await LogoutUser();
+      Alert.alert("Logout successful");
+      router.replace("/(auth)/login");
+    }
+    catch (error: any) {
+      console.error("Logout failed:", error);
+      Alert.alert("Logout failed. Please try again.");
+
+    }
+  }
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -45,6 +62,9 @@ export default function HomeScreen() {
           <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
+        <View>
+          <Button title="Logout" onPress={handleLogout} /> 
+        </View>
       </ThemedView>
     </ParallaxScrollView>
   );
--- app/index.tsx
+++ app/index.tsx
@@ -6,25 +6,31 @@ import Spinner from 'react-native-loading-spinner-overlay';
 export default function Index() {
   const [isLogged, setIsLogged] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+  const [successMessage, setSuccessMessage] = useState("");
  
   useEffect(() => {
     const checkLoginStatus = async () => {
       try {
         const session = await AsyncStorage.getItem("userSession");
-        setIsLogged(session !== null);
+        if (session !== null) {
+          setIsLogged(true);
+          setSuccessMessage('You have successfully logged in!');
+        } else {
+          setIsLogged(false);
+        }
       } catch (error) {
         console.error("Failed to check login status:", error);
         setIsLogged(false);
       } finally {
         setIsLoading(false);
       }
-      }
+    };
 
     checkLoginStatus();
   }, []);
 
   if (isLoading) {
-    return <Spinner visible={true} textContent={'Loading...'} textStyle={{color: "#FFF"}} />;
+    return <Spinner visible={true} textContent={'Loading...'} textStyle={{color: "#000000"}} />;
   }
 
   if (isLogged) {
